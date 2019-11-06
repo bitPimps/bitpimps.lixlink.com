63dd39e4d5c0774d36525df8ca03962e44d921bd
bitPimps <bitpimps@gmail.com>
2019-11-05 22:42:53 -0600

    bitpimps theme
    Adding bitpimps theme to 1.6.07

---
+++ wwwRoot/pages/phpGallery/themes/bitpimps/header.php
@@ -0,0 +1,17 @@
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td>
+<div id="container">
+	<div id="header">
+		<div id="logo"><a href="/"><img src="/images/bitpimps.gif" alt="bitPimps Custom Modifications" name="img_logo" width="170" height="72" border="0" id="img_logo" /></a></div>
+		<div id="nav">
+			<ul>
+				<li><a href="/index.php" title="Home">Home</a></li>
+				<li><a href="/articles/index.php" title="Articles">Articles</a></li>
+				<li><a href="index.php" title="Galleries" style="color:#FFDDAB; background-color:#929292;">Galleries</a></li>
+				<li><a href="/pages/phpForum/index.php" title="Forums">Forums</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="container_body">
+		<div id="content">
