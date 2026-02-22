ffef1e2f2b6eb14318ab624542cea537fb24e5c7
bitPimps <bitpimps@gmail.com>
2026-02-22 14:26:38 -0600

    Delete archive directory

--- archive/wwwRoot2002/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-Header("Content-type: image/jpeg");
-$image = ImageCreate(200, 150);
-$gray = ImageColorAllocate($image, 204, 204, 204);
-$blue = ImageColorAllocate($image, 0, 0, 255);
-ImageLine($image, 10, 10, 150, 30, $blue);
-ImageJPEG($image);
-ImageDestroy($image);
-?>
--- archive/wwwRoot2007/articles/wp-content/themes/typoxp/Copy of footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-<div id="footer">
-	<div class="copyright"><a href="http://www.sunaryohadi.info/typoxp-theme.htm"><strong>TypoXP</strong> theme</a> modified by <a href="http://www.lixlink.com/" title="lixlink.com">lixlink.com</a>. 
-	<?php bloginfo('name'); ?> is powered by <a href="http://wordpress.org/">WordPress</a></div>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
